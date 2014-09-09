044a472b3e82c2fc0e82ec952286d1f5c88705b7
miyucy <fistfvck@gmail.com>
2014-09-09 14:31:54 +0900

    fix spec

--- spec/test-mysqld_spec.rb
+++ spec/test-mysqld_spec.rb
@@ -1,29 +1,11 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
 describe Test::Mysqld do
-  describe ".auto_start" do
-    subject { Test::Mysqld }
-    its(:auto_start) { should be_false }
-
-    context "after set true" do
-      before { Test::Mysqld.auto_start = true }
-      its(:auto_start) { should be_true }
-    end
-
-    describe "should be true or false" do
-      it "set Integer" do
-        Test::Mysqld.auto_start = 1
-        Test::Mysqld.auto_start.should be_true
-      end
-
-      it "set String" do
-        Test::Mysqld.auto_start = "false"
-        Test::Mysqld.auto_start.should be_true
-      end
-
-      it "set nil" do
-        Test::Mysqld.auto_start = nil
-        Test::Mysqld.auto_start.should be_false
+  describe '.auto_start=' do
+    it 'works' do
+      [[true, true], [false, false], [nil, false], [1, true], ['false', true], [Object.new, true]].each do |rvar, expect|
+        Test::Mysqld.auto_start = rvar
+        expect(Test::Mysqld.auto_start).to eq expect
       end
     end
   end
